53b420b430751fce9acaf4b13558e89ef36bdd6a
Cleber Gasparoto <chgasparoto@gmail.com>
2024-08-07 22:53:47 +0200

    feat: implement validation middleware

--- backend/routes/book.routes.js
+++ backend/routes/book.routes.js
@@ -6,13 +6,14 @@ import {
   getBooks,
   updateBook,
 } from "../controllers/book.controller.js";
+import { validateBook } from "../middlewares/book.validate.js";
 
 const router = Router();
 
 router.get("/", getBooks);
 router.get("/:id", getBook);
-router.post("/", createBook);
-router.put("/:id", updateBook);
+router.post("/", validateBook, createBook);
+router.put("/:id", validateBook, updateBook);
 router.delete("/:id", deleteBook);
 
 export default router;
