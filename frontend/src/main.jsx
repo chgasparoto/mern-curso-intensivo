89888f27bf92b295e9542fc754bfb55489f9fffa
Cleber Gasparoto <chgasparoto@gmail.com>
2024-08-07 23:26:22 +0200

    feat: add dark theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { ThemeProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import App from "./App.jsx";
+import createAppTheme from "./theme.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={createAppTheme("dark")}>
+      <App />
+      <CssBaseline />
+    </ThemeProvider>
   </StrictMode>
 );
