f11beba84719ea0ce7f122d0cff34d57674b418a
Cleber Gasparoto <chgasparoto@gmail.com>
2024-08-07 23:42:42 +0200

    feat: add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,35 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+// import Brightness7Icon from "@mui/icons-material/Brightness7";
+
 function App() {
-  return <di>Hello World</di>;
+  return (
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuBookIcon />
+          </IconButton>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            MERN Biblioteca
+          </Typography>
+          <IconButton>
+            <AddBoxIcon />
+          </IconButton>
+          <IconButton>
+            <Brightness4Icon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default App;
