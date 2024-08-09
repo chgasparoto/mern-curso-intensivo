95701aabde02fa961f9de1bd03fdf44746de03c6
Cleber Gasparoto <chgasparoto@gmail.com>
2024-08-09 13:40:06 +0200

    feat: add book skeleton

--- frontend/src/components/BookGrid.jsx
+++ frontend/src/components/BookGrid.jsx
@@ -1,25 +1,44 @@
 import Grid from "@mui/material/Unstable_Grid2";
+import { Button, Container, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 import BookCard from "./BookCard";
 import { useBooks } from "../lib/queries";
+import BookCardSkeleton from "./BookCardSkeleton";
 
 const BookGrid = () => {
   const { isLoading, isError, data } = useBooks();
 
-  if (isLoading) {
-    return <p>Carregando...</p>;
-  }
-
   if (isError) {
-    return <p>Ocorreu um erro ao carregar os livros</p>;
+    return (
+      <Typography paragraph color="error">
+        Ocorreu um erro ao carregar os livros
+      </Typography>
+    );
   }
 
   return (
     <Grid container spacing={5}>
-      {data.length ? (
+      {isLoading ? (
+        <BookCardSkeleton count="4" />
+      ) : data.length ? (
         data.map((book) => <BookCard key={book._id} {...book} />)
       ) : (
-        <p>Não há livros cadastrados</p>
+        <Grid xs={12}>
+          <Container maxWidth="sm" sx={{ textAlign: "center", mt: 2 }}>
+            <Typography variant="h4" component="h5">
+              Nenhum livro cadastrado
+            </Typography>
+            <Button
+              variant="contained"
+              component={Link}
+              to={"/create-book"}
+              sx={{ mt: 2 }}
+            >
+              Cadastrar livro
+            </Button>
+          </Container>
+        </Grid>
       )}
     </Grid>
   );
