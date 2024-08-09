ac2f454ed9335b811ca8a806118e4fb1318f6f86
Cleber Gasparoto <chgasparoto@gmail.com>
2024-08-09 14:21:44 +0200

    feat: server fe from be

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,38 @@
 import "dotenv/config";
+
+import path from "path";
 import express from "express";
 
 import { connectToDatabase } from "./config/db.js";
 import bookRouter from "./routes/book.routes.js";
 
+const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  setTimeout(next, 1500); // Add a 1,5 seconds delay
+app.get("/health", (req, res) => {
+  try {
+    res.status(200).send("Server is running");
+  } catch (error) {
+    console.error("Error checking server health:", error);
+    res.status(500).send("Server is not running");
+  }
 });
 
 app.use("/api/v1/books", bookRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
   connectToDatabase();
 });
